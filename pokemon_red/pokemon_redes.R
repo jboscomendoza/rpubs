8eaee7ea725a0294021eea6704fea520ba1d54f2
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2018-11-14 18:43:58 -0600

    Update pokemon_red/pokemon_redes.R
    Red de habilidades.

--- pokemon_red/pokemon_redes.R
+++ pokemon_red/pokemon_redes.R
@@ -5,23 +5,6 @@ library(ggraph)
 
 pokemon <- read_csv("pokemon.csv")
 
-pokemon$abilities[pokemon$name == "Pikachu"]
-
-pokemon %>%
-  select(name, abilities) %>%
-  mutate_at("abilities", function(x) {
-
-    gsub("(\\[|\\'|\\]|,)+", "_", x) %>%
-      gsub("^_|_$", "", .) %>%
-      gsub("_ _", "_", .) %>%
-      trimws()
-  }
-  ) %>%
-  separate(col = "abilities", into = c(letters[1:6]), sep = "_") %>%
-  mutate_at(letters[1:6], as.factor) %>%
-  gather(orden, nombre, a:f)
-
-
 pokemon %>%
   filter(is.na(type2)) %>%
   select(tipo = type1, starts_with("against")) %>%
@@ -114,15 +97,60 @@ poke_fuerza %>%
   scale_edge_color_manual(values = c("#ddbbbb", "#00ccbb"))
 
 
-pokemon %>% 
+pokemon %>%
   count(type1, type2) %>%
-  filter(n > 3) %>% 
-  na.omit() %>% 
-  graph_from_data_frame() %>% 
+  filter(n > 3) %>%
+  na.omit() %>%
+  graph_from_data_frame() %>%
   ggraph(layout = "linear", circular = TRUE) +
-  geom_edge_arc(aes(color = n, end_cap = label_rect(node2.name)), 
+  geom_edge_arc(aes(color = n, end_cap = label_rect(node2.name)),
                  arrow = arrow(length = unit(1.5, "mm"))) +
   geom_node_label(aes(label = name)) +
   theme_void()
 
-                 
+
+pokemon$abilities[pokemon$name == "Starmie"]
+
+pokemon_habilidades <-
+  pokemon %>%
+  select(name, type1, type2, abilities) %>%
+  mutate_at("abilities", function(x) {
+    gsub("(\\[|\\'|\\]|,)+", "_", x) %>%
+      gsub("^_|_$", "", .) %>%
+      gsub("_ _", "_", .) %>%
+      trimws()
+  }) %>%
+  separate(col = "abilities", into = c(letters[1:6]), sep = "_") %>%
+  mutate_at(letters[1:6], as.factor) %>%
+  gather("Orden_H", "Habilidad", a:f) %>%
+  gather("Orden_T", "Tipo", type1, type2) %>%
+  select(Tipo, Habilidad, name) %>%
+  na.omit()
+
+pokemon_habilidades %>%
+  count(Tipo, Habilidad) %>%
+  graph_from_data_frame() %>%
+  ggraph() +
+  geom_edge_link() +
+  geom_node_point()
+
+pokemon_habilidades %>%
+  count(Tipo, Habilidad) %>%
+  group_by(Tipo) %>%
+  mutate(prop = n / sum(n)) %>%
+  filter(prop >= .05) %>%
+  graph_from_data_frame() %>%
+  ggraph() +
+  geom_edge_link(aes(end_cap = circle(radius = 2, unit = "mm"),
+                     color = prop, alpha = prop, edge_width = prop),
+                 arrow = arrow(type = "open", length = unit(1.5, "mm")),
+                 show.legend = FALSE) +
+  scale_edge_color_continuous(low = "red", high = "blue") +
+  scale_edge_width(range = c(.01, 1.3)) +
+  scale_edge_alpha(range = c(.3, 1)) +
+  geom_node_point(size = 4) +
+  geom_node_text(aes(label = name), repel = TRUE) +
+  theme_void()
+
+
+
